6fd727026ecef5ee0411cc09dda17c36804be637
liaoliang <liao0007@gmail.com>
2017-06-28 13:45:32 +0800

    up

--- dist.sh
+++ dist.sh
@@ -6,8 +6,8 @@ sbt dist
 
 # unzip
 ./dist-play-app-initd.sh stop
-unzip -o ../target/universal/com-youleligou-360wallpaper-1.0-SNAPSHOT.zip -d /var/www/
-rm ../target/universal/com-youleligou-360wallpaper-1.0-SNAPSHOT.zip
+unzip -o ./target/universal/com-youleligou-360wallpaper-1.0-SNAPSHOT.zip -d /var/www/
+rm ./target/universal/com-youleligou-360wallpaper-1.0-SNAPSHOT.zip
 
 # restart
 ./dist-play-app-initd.sh start
